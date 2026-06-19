fc3c78f3088ba03e9b9e80ae3bbafeecda6b6963
LearningBits <hannahbeckmann26a@hotmail.de>
2026-06-19 12:14:33 +0200

    Auslagerung als Methode: Berechnung Gesamtumsatz/PAL_Wert, Festlegung der Altersgruppe sowie Ausgabe Profildaten, Ausgabe BMI Auswertung(Tabelle), Ausgabe Energiebedarf Auswertung, Ausgabe Makronährstoffverteilung Auswertung(Tabelle)
    Interpretieren/Verarbeiten und die Ausgabe der Auswertung sind vollständig als Methoden ausgelagert.

--- src/fitness_assistent_reworked.java
+++ src/fitness_assistent_reworked.java
@@ -3,24 +3,23 @@ import java.util.Scanner;
 public class fitness_assistent_reworked {
 
 	public static void main(String[] args) {
-		// Hier startet der überarbeitete Code des Fitness Assistenten
 		
-	//(1) Begrüßung
+// AB HIER STARTET EINGABEBEREICH
+    //(1) Begrüßung
 	System.out.println("Hallo, ich bin dein Fitnessassistent. Wie heißt du?");
 	Scanner scanner = new Scanner(System.in);
 	String name = scanner.nextLine();
 	System.out.println("");
 	System.out.println("Hey "+name+", hier kannst du deinen BMI (Body-Mass-Index) berechnen.");
 
-	
-	//(2) Abfrage Größe
+    //(2)Abfrage Größe
 	System.out.println("Wie größ bist du? (z.B 1.64 m)");
 			
-	//(2) Variablen deklarieren
+    //Variablen für Größe deklarieren
     double Größe;
     String größe;
 	    
-	//(2) Try/catch Eingabevalidierung
+    //(2)Try/catch Eingabevalidierung
 	try
     { größe = scanner.nextLine();//scannen
 	  größe = pruefeGroeße(größe);
@@ -54,16 +53,15 @@ public class fitness_assistent_reworked {
 		 }
 	  }	
     
-	//(3) Abfrage Gewicht
+    //(3) Abfrage Gewicht
 	System.out.println("");
 	System.out.println("Wieviel wiegst du? (Gib dein Gewicht in kg an)");
 		
-	//(3) Variable deklarieren
+    //Variable deklarieren
 	double Gewicht;
 	String gewicht;
 	
-	
-	//(3) Try/Catch Eingabevalidierung
+    //(3) Try/Catch Eingabevalidierung
 	try 
 	{ gewicht = scanner.nextLine();
 	  gewicht = pruefeGewicht(gewicht);
@@ -79,14 +77,14 @@ public class fitness_assistent_reworked {
 	  Gewicht = Double.parseDouble(gewicht);
 		}
     
-	//(4) Abfrage Alter                               
+    //(4) Abfrage Alter                               
 	System.out.println("");
 	System.out.println("Wie alt bist du? (Runde dein Alter auf eine ganze Zahl auf)");
 		
-	//(4) Variable deklarieren
+	//Variable deklarieren
 	int alter;
 	
-	//(4) Eingabevalidierung try/catch
+    //(4) Eingabevalidierung try/catch
 	try 
 	{ alter = scanner.nextInt();
 	  if (alter<19||alter>99)//Wenn Eingabe außerhalb 19 bis 99 liegt
@@ -105,7 +103,7 @@ public class fitness_assistent_reworked {
 		
 	scanner.nextLine();//scanner leeren
     
-	//(5) Abfrage Geschlecht
+    //(5) Abfrage Geschlecht
 	System.out.println("");
 	System.out.println("Was möchtest du für ein Geschlecht angeben?");
 	System.out.println("Folgende Auswahlmöglichkeiten hast du:");
@@ -116,13 +114,12 @@ public class fitness_assistent_reworked {
 	System.out.println("");
 	System.out.println("Bitte gib jetzt den für dich passenden Buchstaben ein:");
 	
-	//(5) Variable deklarieren
+	//Variable deklarieren
 	String geschlecht;
 	
-	//(5) Antwort scannen
+    //(5) Wertezuweisung if else Anweisung        
 	geschlecht = scanner.nextLine();
 	
-	//(5) if else Anweisung für die Wertezuweisung        
 	if (geschlecht.equalsIgnoreCase("m"))
 	{geschlecht = "männlich";}
 	else if (geschlecht.equalsIgnoreCase("w"))
@@ -130,7 +127,7 @@ public class fitness_assistent_reworked {
 	else 
 	{geschlecht = "divers";}
 	
-	//(6) Abfrage PAL Wert Aktivitätsfaktor
+    //(6) Abfrage PAL Wert Aktivitätsfaktor
 	System.out.println("");
 	System.out.println("Wähle dein ungefaires Aktivitätsniveau.");
 	System.out.println("1 - Nur sitzend, kaum Freizeitaktivität zB Bürojob");
@@ -140,10 +137,10 @@ public class fitness_assistent_reworked {
 	System.out.println("");
 	System.out.println("Bitte gib jetzt die für dich passende Zahl ein:");
 	
-	//(6) Variable für PAL Wert deklarieren
+	//Variable für PAL Wert deklarieren
 	int PAL;
 	
-	//(6) Eingabenvalidierung
+    //(6) Eingabenvalidierung try/catch
 	try 
 	{ PAL = scanner.nextInt();//Antwort scnannen
 	  while (PAL<1||PAL>4)//Wenn eingegebener Zahlenwert außerhalb 1 - 4 liegt
@@ -166,22 +163,89 @@ public class fitness_assistent_reworked {
 	    }
 	}
 	
-	//AB HIER WERTE INTERPRETIEREN UND VERARBEITEN
-	
+//AB HIER WERTE INTERPRETIEREN UND VERARBEITEN
 	//(7) BMI berechnen
-	double bmi;
-	bmi = berechneBMI (Gewicht,Größe); 
+	double bmi = berechneBMI (Gewicht,Größe); 
 	
-	//(7) Altersgruppe festlegen
-	String Altersgruppe;
+	//(8) Altersgruppe festlegen
+	String Altersgruppe = bestimmeAltersgruppe(alter);
 	
-	String AltersgruppeA = "Altersgruppe 19 bis 24 Jahre";
+	//(9) PAL_Wert ermitteln zur Berechnung des Leistungsumsatzes
+	double PAL_Wert = berechnePALwert(PAL);
+	
+	//(10) Grundumsatz berechnen 
+	int Grundumsatz = berechneGrundumsatz(Gewicht,Größe,alter,geschlecht);
+	
+	//(11) Leistungsumsatz berechnen
+	int Leistungsumsatz = berechneLeistungsumsatz(Grundumsatz,PAL_Wert);
+	
+	//(12) Gesamtumsatz berechnen
+	int Gesamtumsatz = Grundumsatz+Leistungsumsatz;
+	
+//AB HIER AUSGABE
+	// Ausgabe Profil
+	AusgabeProfil(name,alter,Größe,Gewicht,geschlecht);
+	
+	// Ausgabe der BMI-Tabelle
+	AusgabeBMItabelle(Altersgruppe,bmi,Größe);
+				
+    // Angabe Infos+Energiebedarf
+	AusgabeEnergiebedarf(Grundumsatz,Leistungsumsatz,Gesamtumsatz,geschlecht);
+	
+	//Ausgabe Makros-Tabelle durch Methode
+	AusgabeMakrotabelle(Gesamtumsatz);
+}
+
+//AB HIER METHODEN	
+//Methoden zum Bereich "Eingabe"
+	//(2)
+public static String pruefeGroeße(String größe)
+{ 	if (größe.contains(" m"))//wenn Eingabe Maßeinheit + Leerzeichen beinhaltet
+     {  größe = größe.replace(" m","");//Maßeinheit entfernen
+	    }
+    else
+	{  	größe = größe.replace("m","");
+		}   
+   
+	if (größe.contains(","))//wenn Komma eingegeben ist
+	{	größe = größe.replace(",",".");//Komma durch Punkt ersetzen
+		}
+	return größe;
+}
+    //(3)
+public static String pruefeGewicht(String gewicht)
+{  if (gewicht.contains(" kg"))//if else Anweisung wenn Maßeinheit mit Leerzeichen oder/und Komma statt Punkt eingegeben ist
+	{  gewicht = gewicht.replace(" kg","");//Maßeinheit löschen
+	   }
+
+   else
+	{  gewicht = gewicht.replace("kg","");//Wenn Maßeinheit ohne Leerzeichen oder/und Komma statt Punkt eingegeben ist/Maßeinheit löschen
+	   }
+
+   if (gewicht.contains(","))//wenn Komma eingegeben ist
+	{  gewicht = gewicht.replace(",",".");//Komma durch Punkt ersetzen
+	   }
+   return gewicht;
+}
+//Methoden zum Bereich "Interpretieren und Verarbeiten"
+    //(7)
+public static double berechneBMI (double Gewicht, double Größe)
+{ double bmi;
+  bmi = Gewicht/(Größe*Größe);
+  bmi = Math.round(bmi*100.0)/100.0;//Auf zwei Nachkommastellen runden.
+  return bmi;
+}
+    //(8)
+public static String bestimmeAltersgruppe(int alter)
+{   String AltersgruppeA = "Altersgruppe 19 bis 24 Jahre";
 	String AltersgruppeB = "Altersgruppe 25 bis 34 Jahre";
 	String AltersgruppeC = "Altersgruppe 35 bis 44 Jahre";
 	String AltersgruppeD = "Altersgruppe 45 bis 54 Jahre";
 	String AltersgruppeE = "Altersgruppe 55 bis 64 Jahre";
 	String AltersgruppeF = "Altersgruppe ab 65 Jahre";
-
+	
+	String Altersgruppe;
+	
 	//(7) Altersgruppe bestimmen
 	if (alter<=24)
 	{ Altersgruppe = AltersgruppeA;
@@ -202,14 +266,15 @@ public class fitness_assistent_reworked {
 	{ Altersgruppe = AltersgruppeF;
 	}	
 	
-	//(7) BMI Werte festlegen
+	return Altersgruppe;
+}
+public static double ordneBMIderAltersgruppezu(String Altersgruppe, double bmi)
+{	String AltersgruppeA = "Altersgruppe 19 bis 24 Jahre";
+	String AltersgruppeB = "Altersgruppe 25 bis 34 Jahre";
+	String AltersgruppeC = "Altersgruppe 35 bis 44 Jahre";
+	String AltersgruppeD = "Altersgruppe 45 bis 54 Jahre";
+	String AltersgruppeE = "Altersgruppe 55 bis 64 Jahre";
 	double BMInormalgewicht_ug;
-	double BMInormalgewicht_og;
-	double BMIübergewicht_ug;
-	double BMIübergewicht_og;
-	double BMIadipositas;
-	
-	//(8) BMI Wert abhängig vom Alter zuordnen 
 	
 	if (Altersgruppe==AltersgruppeA)
 	{ BMInormalgewicht_ug = 19.00;
@@ -230,43 +295,11 @@ public class fitness_assistent_reworked {
 	{ BMInormalgewicht_ug = 24.00;
 	  }
 	
-	//(8) BMI Werte für die Tabelle errechnen
-	  BMInormalgewicht_og = berechnehöherenBMI(BMInormalgewicht_ug);
-	  BMIübergewicht_ug   = BMInormalgewicht_og+0.1;//Untere Grenze der nächsthöheren klasse sind plus 0.1
-	  BMIübergewicht_og   = BMInormalgewicht_og+5;//
-	  BMIadipositas       = BMIübergewicht_og+0.1;
-	
-	//(8) Gewichte für die Tabelle errechnen
-	double normalgewicht_ug = BMInormalgewicht_ug*(Größe*Größe); 
-	normalgewicht_ug = Math.round(normalgewicht_ug*10)/10;
-	
-	double normalgewicht_og = BMInormalgewicht_og*(Größe*Größe);
-	normalgewicht_og = Math.round(normalgewicht_og*10)/10;
-	
-	double übergewicht_ug = normalgewicht_og+0.1;
-	
-	double übergewicht_og = BMIübergewicht_og*(Größe*Größe);
-	übergewicht_og = Math.round(übergewicht_og*10)/10;
-	
-	double adipositas = übergewicht_og+0.1;
-	
-	//(8) BMI Wert der passenden Kategorie zuordnen
-	String einordnungBMI;
-	
-	if (bmi<BMInormalgewicht_ug)
-	{ einordnungBMI = " → Untergewicht";}
-				
-	else if (bmi<=BMInormalgewicht_og)
-	{ einordnungBMI = " → Normalgewicht";}
-				
-	else if (bmi<=BMIübergewicht_og)
-	{ einordnungBMI = " → Übergewicht";}
-				
-	else 
-	{ einordnungBMI = " → Adipositas";}
-
-	//(9) Aktivitätsfaktor PAL dem entsprechenden Wert zuordnen 
-	double PAL_Wert;//Variable zur Einordnung der Angabe zum Aktivitätsniveau
+	return BMInormalgewicht_ug;
+}
+    //(9)
+public static double berechnePALwert(int PAL)
+{   double PAL_Wert;
 	if (PAL==1)
 	{ PAL_Wert = 1.4;} 
 	else if (PAL==2)
@@ -276,20 +309,36 @@ public class fitness_assistent_reworked {
 	else
 	{ PAL_Wert = 2.0;}
 	
-	//(9) Grundumsatz berechnen 
-	int Grundumsatz;
-	Grundumsatz = berechneGrundumsatz(Gewicht,Größe,alter,geschlecht);
+	return PAL_Wert;
+}
+   //(10)
+
+public static int berechneGrundumsatz (double Gewicht,double Größe,int alter,String geschlecht)
+{
+	double Größe_cm = Größe*100;
+	double Grundumsatz_d = (10*Gewicht)+(6.25*Größe_cm)-(5*alter);
 	
-	//(9) Leistungsumsatz berechnen
-	int Leistungsumsatz;
-	Leistungsumsatz = berechneLeistungsumsatz(Grundumsatz,PAL_Wert);
+	if (geschlecht=="männlich")
+	{ Grundumsatz_d = Grundumsatz_d+5;}
+	else if (geschlecht=="weiblich")
+	{ Grundumsatz_d = Grundumsatz_d-161;}
+	else
+	{ Grundumsatz_d = Grundumsatz_d-83;}//Bei Eingabe divers wird der Mittelwert berechnet
 	
-	//(9) Gesamtumsatz berechnen
-	int Gesamtumsatz = Grundumsatz+Leistungsumsatz;
+    int Grundumsatz = (int)Grundumsatz_d;//Alles hinter dem Komma wird "abgeschnitten"
+    
+    return Grundumsatz ;
+}
+    //(11)
+public static int berechneLeistungsumsatz(int Grundumsatz, double PAL_Wert)
+{   double Leistungsumsatz_d = Grundumsatz*(PAL_Wert-1);
+    int Leistungsumsatz = (int)Leistungsumsatz_d;//alles hinterm Komma wird "abgeschnitten"
 	
-	// AB HIER AUFLISTUNG
-	
-	// Aufführung der eingegebenen Parameter
+    return Leistungsumsatz;
+}
+//Methoden zum Bereich Ausgabe
+public static void AusgabeProfil(String name, int alter, double Größe,double Gewicht, String geschlecht)
+{
 	System.out.println("");
 	System.out.println("");
 	System.out.println("————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————");
@@ -304,7 +353,48 @@ public class fitness_assistent_reworked {
 	System.out.println("                  Geschlecht : "+geschlecht);
 	System.out.println("");
 	
-	//(8) Tabelle BMI ausgeben
+}
+public static double berechnehöherenBMI (double BMInormalgewicht_ug)
+{  double nächsteKlasse = BMInormalgewicht_ug+5;
+   return nächsteKlasse;}
+public static void AusgabeBMItabelle(String Altersgruppe,double bmi,double Größe)
+{
+    double BMInormalgewicht_ug = ordneBMIderAltersgruppezu (Altersgruppe,bmi);//BMI Wert abhängig vom Alter zuordnen
+	//BMI Werte für die Tabelle errechnen
+	double BMInormalgewicht_og = berechnehöherenBMI(BMInormalgewicht_ug);
+	double BMIübergewicht_ug   = BMInormalgewicht_og+0.1;//Untere Grenze der nächsthöheren klasse sind plus 0.1
+	double BMIübergewicht_og   = BMInormalgewicht_og+5;//
+	double BMIadipositas       = BMIübergewicht_og+0.1;
+		
+    //Gewichte für die Tabelle errechnen
+	double normalgewicht_ug = BMInormalgewicht_ug*(Größe*Größe); 
+	normalgewicht_ug = Math.round(normalgewicht_ug*10)/10;
+		
+	double normalgewicht_og = BMInormalgewicht_og*(Größe*Größe);
+	normalgewicht_og = Math.round(normalgewicht_og*10)/10;
+		
+	double übergewicht_ug = normalgewicht_og+0.1;
+		
+	double übergewicht_og = BMIübergewicht_og*(Größe*Größe);
+	übergewicht_og = Math.round(übergewicht_og*10)/10;
+		
+	double adipositas = übergewicht_og+0.1;
+		
+    //BMI Wert der passenden Kategorie zuordnen
+	String einordnungBMI;
+		
+	if (bmi<BMInormalgewicht_ug)
+	{ einordnungBMI = " → Untergewicht";}
+					
+	else if (bmi<=BMInormalgewicht_og)
+	{ einordnungBMI = " → Normalgewicht";}
+					
+	else if (bmi<=BMIübergewicht_og)
+	{ einordnungBMI = " → Übergewicht";}
+					
+	else 
+	{ einordnungBMI = " → Adipositas";}
+		
 	System.out.println("");
 	System.out.println("                  Dein BMI   : "+bmi+einordnungBMI);
 	System.out.println("                  Der BMI für die "+Altersgruppe+" wird folgendermaßen interpretiert:");
@@ -317,8 +407,10 @@ public class fitness_assistent_reworked {
 	System.out.println("                      Übergewicht    "+BMIübergewicht_ug+" - "+BMIübergewicht_og+          "  ⁞ von   "+übergewicht_ug+" - "+übergewicht_og+" Kg");
 	System.out.println("                      Adipositas    >"+BMIadipositas+                               "         ⁞ ab    "+adipositas+" Kg");
 	System.out.println("                  …………………………………………………………………………………………………………………………………………     ");
-				
-    //(9) Angabe zum Grundumsatz
+}
+
+public static void AusgabeEnergiebedarf(int Grundumsatz,int Leistungsumsatz, int Gesamtumsatz, String geschlecht)
+{
 	System.out.println("");
 	if (geschlecht=="divers")
    {System.out.println("         [ Info ] Die Mifflin-St. Jeor-Formel berechnet den Grundumsatz anhand des angegebenen Geschlechts.");
@@ -338,86 +430,9 @@ public class fitness_assistent_reworked {
 	System.out.println("");
 	System.out.println("                  Dein Gesamtumsatz beträgt    → "+Gesamtumsatz+" kcal pro Tag (Gesamter Energiebedarf)");
 	System.out.println("");
-	System.out.println("");
-	
-	//(9) Ausgabe der Tabelle durch Methode
-	berechneMakros(Gesamtumsatz);
-	System.out.println("————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————");
-	System.out.println("");
-	System.out.println("");
-	 
-	
-	
-	}//Schließt main ab
-	
-public static String pruefeGroeße(String größe)//prüft ob Leerzeichen und/oder "m" eingegeben wurde
-{ 	if (größe.contains(" m"))//if else Anweisung wenn Eingabe Maßeinheit + Leerzeichen beinhaltet
-     {  größe = größe.replace(" m","");//Maßeinheit entfernen
-		größe = größe.toUpperCase();//Wert nach Entfernung der Maßeinheit neu zuweisen
-     	}
-
-	else
-	{  	größe = größe.replace("m","");
-		größe = größe.toUpperCase();
-		}   
-   
-	if (größe.contains(","))//wenn Komma eingegeben ist
-	{	größe = größe.replace(",",".");//Komma durch Punkt ersetzen
-		größe = größe.toUpperCase();//Variable neu zuordnen
-		}
-	return größe;
+	System.out.println("");	
 }
-public static String pruefeGewicht(String gewicht)
-{  if (gewicht.contains(" kg"))//if else Anweisung wenn Maßeinheit mit Leerzeichen oder/und Komma statt Punkt eingegeben ist
-	{  gewicht = gewicht.replace(" kg","");//Maßeinheit löschen
-	   gewicht = gewicht.toUpperCase();//Variable neu zuordnen
-		}
-
-   else
-	{  gewicht = gewicht.replace("kg","");//Wenn Maßeinheit ohne Leerzeichen oder/und Komma statt Punkt eingegeben ist/Maßeinheit löschen
-	   gewicht = gewicht.toUpperCase();//Variable neu zuordnen
-		}
-
-   if (gewicht.contains(","))//wenn Komma eingegeben ist
-	{  gewicht = gewicht.replace(",",".");//Komma durch Punkt ersetzen
-	   gewicht = gewicht.toUpperCase();//Variable neu zuordnen
-		}
-   return gewicht;
-}
-public static double berechnehöherenBMI (double BMInormalgewicht_ug)
-{  double nächsteKlasse = BMInormalgewicht_ug+5;
-   return nächsteKlasse;
-}
-public static double berechneBMI (double Gewicht, double Größe)
-{ double bmi;
-  bmi = Gewicht/(Größe*Größe);
-  bmi = Math.round(bmi*100.0)/100.0;//Auf zwei Nachkommastellen runden.
-
-  return bmi;
-}
-public static int berechneGrundumsatz (double Gewicht,double Größe,int alter,String geschlecht)
-{
-	double Größe_cm = Größe*100;
-	double Grundumsatz_d = (10*Gewicht)+(6.25*Größe_cm)-(5*alter);
-	
-	if (geschlecht=="männlich")
-	{ Grundumsatz_d = Grundumsatz_d+5;}
-	else if (geschlecht=="weiblich")
-	{ Grundumsatz_d = Grundumsatz_d-161;}
-	else
-	{ Grundumsatz_d = Grundumsatz_d-83;}//Bei Eingabe divers wird der Mittelwert berechnet
-	
-    int Grundumsatz = (int)Grundumsatz_d;//Alles hinter dem Komma wird "abgeschnitten"
-    
-    return Grundumsatz ;
-}
-public static int berechneLeistungsumsatz(int Grundumsatz, double PAL_Wert)
-{   double Leistungsumsatz_d = Grundumsatz*(PAL_Wert-1);
-    int Leistungsumsatz = (int)Leistungsumsatz_d;//alles hinterm Komma wird "abgeschnitten"
-	
-    return Leistungsumsatz;
-}
-public static void berechneMakros(int Gesamtumsatz)
+public static void AusgabeMakrotabelle(int Gesamtumsatz)
 {// Nährstoffzusammensetzung berechnen (Anhand der Richtwerte der DGE)
 	double KH_d = Gesamtumsatz*0.50;//50% Kohlenhydrate
 	int KH = (int)KH_d;
@@ -435,5 +450,10 @@ public static void berechneMakros(int Gesamtumsatz)
 	System.out.println("                  …………………………………………………………………………………………………………………………………………     ");
 	System.out.println("");
 	System.out.println("");
+	System.out.println("————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————");
+	System.out.println("");
+	System.out.println("");
+	 
 }
+
 }//schließt Klasse ab
